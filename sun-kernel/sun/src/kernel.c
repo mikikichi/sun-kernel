f01bac709d2c717012e017d4465a78d9403de8a6
GBX9570 <lr.russell@proton.me>
2025-04-28 20:00:35 +0100

    basic custom GDT implemented WOO OH MY GOD
    not going to be used, more proof of concept than anything cause it does NOT use good practices lmfao

---
+++ sun-kernel/sun/src/kernel.c
@@ -0,0 +1,57 @@
+#include <stdbool.h>
+#include "libs/print.h"
+#include "libs/log.h"
+#include "terminal/terminal.h"
+#include "idt/idt.h"
+#include "terminal/programs/exit.h"
+#include "libs/serial.h"
+
+extern unsigned char pageinc;
+extern unsigned char gdtinc;
+extern unsigned char longinc;
+extern unsigned char idtinc;
+
+void kernel_main(void) 
+{
+
+    clear();
+
+    idt_init();
+
+    serial_init();
+    printf("Serial is up!\n");
+
+    info("Kernel is UP\n");
+
+    if (pageinc == 1) {
+        begin("Paging has started\n");
+    } else {
+        fatal("Paging could not start.\n");
+
+        halt();
+    }
+
+    if (gdtinc == 1) {
+        begin("GDT has loaded\n");
+    } else {
+        fatal("GDT failed\n");
+        halt();
+    }
+
+    if (longinc == 1) {
+        begin("Long mode entered\n");
+    } else {
+        fatal("Long mode failed\n");
+        halt();
+    }
+
+    if (idtinc == 1) {
+        begin("IDT has started\n");
+
+    } else {
+        fatal("IDT could not start.\n");
+        halt();
+    }
+
+    terminal();
+}
