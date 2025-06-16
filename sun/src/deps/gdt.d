5c4133a88a29c6c095c583327365cfd4434a90f3
mikikichi <khp04656@gmail.com>
2025-06-16 20:22:10 +0300

    just stop pushing .o filesgit add .

--- sun/src/deps/gdt.d
+++
@@ -1,8 +0,0 @@
-/home/alibaba/sun-kernel/sun/src/gdt.o: \
- /home/alibaba/sun-kernel/sun/src/gdt.c \
- /home/alibaba/sun-kernel/sun/src/libs/log.h \
- /home/alibaba/opt/cross/lib/gcc/x86_64-elf/15.1.0/include/stdint.h \
- /home/alibaba/opt/cross/lib/gcc/x86_64-elf/15.1.0/include/stdint-gcc.h
-/home/alibaba/sun-kernel/sun/src/libs/log.h:
-/home/alibaba/opt/cross/lib/gcc/x86_64-elf/15.1.0/include/stdint.h:
-/home/alibaba/opt/cross/lib/gcc/x86_64-elf/15.1.0/include/stdint-gcc.h:
